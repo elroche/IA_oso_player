1255f9dfbc39ec953fa7d4cf8064a2fdff734248
QuentinPerret <quentinprrt33@gmail.com>
2023-12-01 15:42:01 +0100

    add script

---
+++ scripts/model_definition.py
@@ -0,0 +1,157 @@
+from PIL import ImageGrab
+import cv2 as cv
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import torch
+
+from torchvision.models.detection import fasterrcnn_resnet50_fpn
+from torchvision.transforms import functional as F
+from torchvision import transforms
+
+import torch.nn.functional as F
+import torch.nn as nn
+
+import pytesseract
+
+import time
+import re
+import os
+
+torch.cuda.is_available()
+
+def flatten_image(contours_image):
+    # Aplatir l'image en une seule colonne
+    flattened_image = contours_image.flatten()
+
+    # Renvoyer la matrice aplatie
+    return flattened_image
+
+class ContourDetector(nn.Module):
+    def __init__(self):
+
+        super(ContourDetector, self).__init__()
+
+        # Couches de convolution
+        self.Conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
+        self.Conv2 = nn.Conv2d(32, 1, kernel_size=3, stride=1, padding=1) # : division de la taille de la matrice de contours par 2, je trouve ça bien comme résultat
+
+        # Couches de pooling
+        self.Pool = nn.MaxPool2d(kernel_size=1, stride=2, padding=0) # division de la taille de la matrice de contours par 4(stride ici a 2 et a la 2e couche de conv stride à 1)
+        self.Flat=nn.Flatten()
+
+
+    def forward(self, x):
+        # Passe avant les couches de convolution
+        x = self.Pool(F.relu(self.Conv1(x)))
+        z = self.Pool(F.relu(self.Conv2(x)))
+
+        y = self.Flat(z)
+        print("Dimensions avec shape:", y.shape)
+
+        return z,y
+
+contour_model = ContourDetector()
+transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize([0.5, ], [0.5, ])])
+print(contour_model)
+
+def detect_numbers(image):
+    # Charger un modèle Faster R-CNN pré-entraîné
+    model = fasterrcnn_resnet50_fpn(pretrained=True)
+    model.eval()
+
+    # Récupérer les dimensions originales de l'image
+    width, height = image.size
+
+    # Définir les coordonnées de la région à traiter (1/4 de la longueur et 1/4 de la hauteur)
+    region_left = int(0.75 * width)
+    region_top = 0
+    region_right = width
+    region_bottom = int(0.25 * height)
+
+    # Extraire la région à traiter
+    region = image.crop((region_left, region_top, region_right, region_bottom))
+    plt.imshow(region)
+    image.show()
+
+    # Appliquer les transformations nécessaires à la région
+    region_tensor = F.to_tensor(region)
+    region_tensor = region_tensor.unsqueeze(0)  # Ajouter une dimension pour le lot
+
+    # Effectuer l'inférence sur la région
+    with torch.no_grad():
+        prediction = model(region_tensor)
+
+    # Convertir la région en niveaux de gris
+    region_gray = region.convert('L')
+
+    # Utiliser Tesseract OCR pour reconnaître le texte dans la région
+    result = pytesseract.image_to_string(region_gray)
+
+    # Filtrer les caractères pour ne conserver que les nombres, le caractère "%", et le retour à a ligne pour séparer les 2 résultats
+    filtered_result = re.sub(r'[^0-9%\n,.]', '', result)
+
+    return prediction, region, filtered_result  # Retourner également le résultat filtré pour une utilisation ultérieure
+
+def extraire_score_precision(filtered_result):
+    # Fonction pour extraire le score et la précision à partir du filtered_result
+
+    # Extraire le score (le score étant sur la première ligne)
+    lignes = filtered_result.split('\n')
+    score_str = lignes[0].strip().split()[-1]
+    score = int(score_str)
+
+    # Extraire la précision (la précision étant sur la deuxième ligne)
+    precision_str = lignes[2].strip().replace('%', '')
+    precision_str = precision_str.replace(',', '.')  # Remplacer la virgule par un point
+    precision = float(precision_str)
+
+    return score, precision
+
+def difference_scores_precisions(filtered_result_precedente, filtered_result_actuelle):
+    # Extraire le score et la précision des prédictions
+    score_precedent, precision_precedente = extraire_score_precision(filtered_result_precedente)
+    score_actuel, precision_actuelle = extraire_score_precision(filtered_result_actuelle)
+
+    # Calculer la différence du score et de la précision
+    difference_score = score_actuel - score_precedent
+    difference_precision = precision_actuelle - precision_precedente
+
+    return difference_score, difference_precision
+
+# Récupération des screenshots
+screenshots = test_recuperation_image()
+image_precedente = screenshots[-8]
+image_actuelle = screenshots[-1]
+
+# Convertir les screenshots en tableau NumPy
+image_precedente_np = np.array(image_precedente)
+image_actuelle_np = np.array(image_actuelle)
+
+# Convertir les screenshots en objet PIL.Image
+pil_image_precedente = Image.fromarray(image_precedente_np)
+pil_image_actuelle = Image.fromarray(image_actuelle_np)
+
+# Appeler la fonction de détection
+prediction_precedente, region_precedente, filtered_result_precedente = detect_numbers(pil_image_precedente)
+prediction_actuelle, region_actuelle, filtered_result_actuelle = detect_numbers(pil_image_actuelle)
+
+# Afficher les résultats de la fonction de prédiction
+print("Résultat de la reconnaissance de nombres de l'image précédente :", filtered_result_precedente)
+print("Résultat de la reconnaissance de nombres de l'image actuelle :", filtered_result_actuelle)
+
+# Extraction du score et de la précision des prédictions faites sur les images (pas obligatoire car fait dans la fonction extraire_score_precision)
+score_actuel, precision_actuelle = extraire_score_precision(filtered_result_actuelle)
+print("Score de l'image actuelle :", score_actuel)
+print("Precision de l'image actuelle :", precision_actuelle)
+score_precedent, precision_precedente = extraire_score_precision(filtered_result_precedente)
+print("Score de l'image précédente :", score_precedent)
+print("Precision de l'image précédente :", precision_precedente)
+
+# Test de la fonction difference_scores_precisions
+difference_score, difference_precision = difference_scores_precisions(filtered_result_precedente, filtered_result_actuelle)
+
+# Afficher les résultats du test, cad de la différence du score et de la précision entre les deux images
+print("Différence du score :", difference_score)
+print("Différence de la précision :", difference_precision)
